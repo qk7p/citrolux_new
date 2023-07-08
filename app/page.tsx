0b25f20e30a44cc441a6d618495f95c2e1246b68
Morozov Dmitry <d.morozov@qk7p.ru>
2023-07-08 16:28:18 +0300

    подготовлен хеддер и блок о продукте

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,119 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className={"relative"}>
+      <img
+        src={"/images/site_long_final_new_2_1280.jpg"}
+        className={"absolute left-0 top-0 -z-10 opacity-30"}
+      />
+      <header className={"h-banner bg-green-50"}>
+        <nav className={"flex justify-between py-5 px-16"}>
+          <div className={"w-60 ml-24 mt-1"}>
+            <img src={"/images/logo-2.png"} className={"w-full h-full"} />
+          </div>
+          <ul
+            className={
+              "w-full flex flex-row justify-end pt-6 uppercase font-proxima-extra-bold text-sm tracking-widest text-main"
+            }
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <li className={""}>
+              <a href="#">где приобрести</a>
+            </li>
+            <li className={"ml-6"}>
+              <a href="#">рекомендации по применению</a>
+            </li>
+            <li className={"ml-6"}>
+              <a href="#">производитель</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main className="">
+        {/* About */}
+        <section className={"relative bg-top bg-no-repeat"}>
+          <div className={"w-full flex justify-around mb-56"}>
+            <img
+              src={"/images/drop.png"}
+              className={"absolute -top-20 z-10 w-44"}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          </div>
+          <div className={"flex flex-row w-2/3 m-auto"}>
+            <div className={"w-5/12 text-gray-600"}>
+              <h4 className={"italic font-proxima-regular text-3xl "}>
+                <span className={"text-main"}>Цитролюкс</span> - забота о себе и
+                близких каждый день.
+              </h4>
+              <h5 className={"font-proxima-regular text-main text-xl mt-4"}>
+                О продукте:
+              </h5>
+              <p className={"mt-2 font-proxima-regular text-md leading-tight"}>
+                Цитролюкс создан на основе экстракта из семян грейпфрута и
+                обогащен витамином С.{" "}
+              </p>
+              <p className={"mt-2 font-proxima-regular text-md leading-tight"}>
+                Экстракт из семня грейпфрута содержит биофлаваноид - нарингин,
+                обладающий уникальными свойствами "природного антибиотика" и
+                антиоксиданта.
+              </p>
+              <p className={"mt-2 font-proxima-regular text-md leading-tight"}>
+                Цитролюкс - натуральный продукт немецкого качества для взрослых
+                и детей старше 12 лет.
+              </p>
+            </div>
+            <div className={"w-3/12 flex items-center"}>
+              <img src={"/images/citrolux.png"} className={""} />
+            </div>
+            <div className={"w-5/12 flex flex-col font-proxima-regular text-gray-600"}>
+              <div className={"self-end"}>
+                <a href="" className={"flex flex-col items-center self-end "}>
+                  <img src="/images/pdf.png" alt="" className={"w-16"} />
+                  <p
+                    className={
+                      "w-40 text-md text-center underline text-main leading-tight"
+                    }
+                  >
+                    Скачать рекомендации по применению
+                  </p>
+                </a>
+              </div>
+              <h5 className={"text-xl text-main"}>Состав:</h5>
+              <p>-Экстракт семян грейфрута</p>
+              <p>-Аскорбиновая кислота</p>
+              <h5 className={"text-xl text-main mt-2"}>
+                Содержание активных компонентов в 1 мл раствора:
+              </h5>
+              <p>-Цитрусовые биофлаваноиды (нарингин) - не менее 8мг/мл</p>
+              <p>-Витамин С 30 мг/мл</p>
+              <h5 className={"text-xl text-main mt-2"}>Форма выпуска:</h5>
+              <p>-Раствор по 20 мл или 50 мл во флаконе</p>
+            </div>
+          </div>
+          <div className={"w-full flex justify-around p-10"}>
+            <div className={"flex flex-col items-center w-24"}>
+              <img src="/images/eco-product.png" alt="" className={"w-24"} />
+              <p
+                className={
+                  "mt-2 font-proxima-extra-bold uppercase text-xl text-center leading-tight tracking-wide text-gray-600"
+                }
+              >
+                Натуральный продукт
+              </p>
+            </div>
+            <div className={"flex flex-col items-center w-24"}>
+              <img src="/images/eco-sources.png" alt="" className={"w-24"} />
+              <p
+                className={
+                  "mt-2 font-proxima-extra-bold uppercase text-xl text-center leading-tight tracking-wide text-gray-600"
+                }
+              >
+                Экологически чистое сырье
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "proxima-extra-bold": ["ProximaExtraBold"],
+        "proxima-regular": ["ProximaRegular"],
+      },
+      height: {
+        banner: "45.9375rem",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "banner-image": "url('/images/banner.jpg')",
+        "about-background": "url('/images/about-background.png')",
+      },
+      colors: {
+        main: "#c8344d",
       },
     },
   },
   plugins: [],
-}
+};
